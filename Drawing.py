d493466ec9dcb7c396e7114c4992b9b0d00f8724
brockttaylor <71046601+brockttaylor@users.noreply.github.com>
2022-02-11 20:03:18 -0500

    Added some documentation

--- Drawing.py
+++ Drawing.py
@@ -1,12 +1,12 @@
 
 import svgpathtools as svg
-from scipy.integrate import trapz, quad as spquad
+from scipy.integrate import trapz
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from quadpy import quad
 
 def complex_quad(func, t_vals):
+    #compute integral of a complex function over the interval t
     real = np.real(func)
     imag = np.imag(func)
     i = 1j
@@ -17,6 +17,7 @@ def complex_quad(func, t_vals):
 
 
 def Fourier(Paths_DF, levels):
+    #parameterization of f as a piecewise function given Paths_DF
     def f(t):
         Filter = Paths_DF["time"].values < t
         loc = Paths_DF.iloc[0, 0]
@@ -26,26 +27,26 @@ def Fourier(Paths_DF, levels):
         return np.polyval(loc.poly(), t)
     superposition = 0
 
-    size = 1000
+    size = 1000 #number of slices from 0 to 1
     i = 1j
     t_vals = np.linspace(0, 1, size)
 
-    vals = np.array(list(map(f, t_vals)))
-
-
-    # for t in t_vals:
-    #     np.append(vals, f(t))
-    #     #print(f(t))
-    #print(vals)
+    vals = np.array(list(map(f, t_vals)))#map the function f over the interval 0 <= t < 1
 
     for n in range (-levels, levels + 1):
 
         exp = np.exp(- 2 * n * np.pi * i * t_vals)
+        #exponential component
 
         c = complex_quad(vals * exp, t_vals)
+        #constant component
+
         superposition += c * exp
+        #summation of each level n
     real = np.real(superposition)
     imag = np.imag(superposition)
+
+    #return real, imag as a tuple
     return real, imag
 
 
@@ -72,12 +73,16 @@ def arc_length(poly, t1, t2):
 
 if __name__ == '__main__':
 
-    paths, attributes, svg_attributes = svg .svg2paths2("witch-by-loginueveilustra-silhouette.svg")
-    #svg.wsvg(paths, attributes = attributes, svg_attributes = svg_attributes, filename="output1.svg")
+    ###TO CHANGE FILES:
+    #add file to the project directory and change the line below this one to match the name of the new file
+    #...svg2paths2("<filename>.svg")
+    paths, attributes, svg_attributes = svg.svg2paths2("witch-by-loginueveilustra-silhouette.svg")
+
     paths = paths[0]
     Paths_DF = pd.DataFrame({"Path": paths, "Length": paths.length()})
     total_length = Paths_DF["Length"].values.sum()
     Paths_DF = Paths_DF.assign(percent_length = Paths_DF["Length"] / total_length).drop(columns = "Length")
+
     #percent length will equal the amount of time that the function should spend on that path
     Paths_DF = Paths_DF.assign(time = Paths_DF["percent_length"].cumsum()).drop(columns = "percent_length")
 
